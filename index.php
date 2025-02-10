fb07bd7949a1216572b6333d92f3b41e9ba3ae7d
iradukun <mustaphairadukunda@gmail.com>
2025-02-10 14:30:28 +0200

    refactor: clean up index.php and update hover color in login form button

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
-// GradeMaster/index.php
 
-// Define the application root directory
 define('APPROOT', dirname(__FILE__) . '/app');
 define('URLROOT', 'http://localhost/GradeMaster');
 define('SITENAME', 'GradeMaster');
